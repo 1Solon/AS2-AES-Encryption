05ab373493c654613d0a78132a8ce429a592a1e8
Saul Burgess <Burgesssaul@gmail.com>
2024-04-08 10:48:37 +0000

    Implements keyExpansion

---
+++ src/functions/keyExpansion/keyExpansion.c
@@ -0,0 +1,39 @@
+#include "keyExpansion.h"
+
+void KeyExpansion(const uint8_t* inputKey, uint8_t* expandedKeys, const uint8_t* s_box, const uint8_t* r_con, const int keyLen, const int totalKeyLen) {
+    int i = 0;
+    uint8_t temp[4];
+
+    // The first round key is the key itself
+    for (i = 0; i < keyLen; i++) {
+        expandedKeys[i] = inputKey[i];
+    }
+
+    // All other round keys are derived from the previous round keys
+    for (; i < totalKeyLen; i += 4) {
+        for (int j = 0; j < 4; j++) {
+            temp[j] = expandedKeys[(i - 4) + j];
+        }
+
+        if (i % keyLen == 0) {
+            // Rotate the 4-byte word
+            uint8_t k = temp[0];
+            temp[0] = temp[1];
+            temp[1] = temp[2];
+            temp[2] = temp[3];
+            temp[3] = k;
+
+            // Apply S-box substitution
+            for (int j = 0; j < 4; j++) {
+                temp[j] = s_box[temp[j]];
+            }
+
+            // XOR with Rcon
+            temp[0] = temp[0] ^ r_con[i / keyLen];
+        }
+
+        for (int j = 0; j < 4; j++) {
+            expandedKeys[i + j] = expandedKeys[i - keyLen + j] ^ temp[j];
+        }
+    }
+}
